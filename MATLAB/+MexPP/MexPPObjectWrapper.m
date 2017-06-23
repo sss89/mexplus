f8d26d08a9ecd9baf726c061fc4179bf4da718d8
Uriel Sinichkin <uriel@rsip.co.il>
2017-06-23 10:17:08 +0300

    implemented MexPPObjectWrapper - an object that wraps single C++ object

--- MATLAB/+MexPP/MexPPObjectWrapper.m
+++ MATLAB/+MexPP/MexPPObjectWrapper.m
@@ -1,22 +1,58 @@
-classdef MexPPObjectWrapper < handle
+classdef MexPPObjectWrapper < matlab.mixin.Copyable
     %MEXPPOBJECTWRAPPER Summary of this class goes here
     %   Detailed explanation goes here
     
     properties
         class_name
         mex_handle
-        id
+%         id
         saved_state
         AppWrapper
     end
     
+    methods(Access = protected)
+        % Override copyElement method:
+        function cpObj = copyElement(obj)
+            to_restore = ~isempty(obj.mex_handle);
+            
+            obj.disconnect();
+            cpObj = copyElement@matlab.mixin.Copyable(obj);
+            
+            if to_restore
+                obj.restoreConnection();
+                cpObj.restoreConnection();
+            end
+        end
+    end
+    
+    methods (Static)
+        function obj = loadobj(s)
+            obj.AppWrapper = MexPP.MexPPAppWrapper.getAppWrapper(s.app_name);
+            obj.class_name = s.class_name;
+            obj.saved_state = s.saved_state;
+            obj.mex_handle = [];
+            if s.is_connected
+                obj.restoreConnection();
+            end
+        end
+        
+    end
+   
     methods
+        function s = saveobj(obj)
+            s = struct();
+            s.app_name = obj.AppWrapper.getAppName();
+            s.class_name = obj.class_name;
+            s.saved_state = obj.getSavedState();
+            s.is_connected = ~isempty(obj.mex_handle);
+        end
+        
         function obj = MexPPObjectWrapper(class_name, mex_handle, AppWrapper)
             obj.class_name = class_name;
             obj.mex_handle = mex_handle;
             obj.AppWrapper = AppWrapper;
             obj.saved_state = [];
-            obj.id = mex_handle;
+%             obj.id = mex_handle;
         end
         
         function varargout = runCommand(obj, func_name, varargin)
@@ -24,20 +60,22 @@ classdef MexPPObjectWrapper < handle
         end
         
         function delete(obj)
-            obj.AppWrapper.deleteObj(obj.mex_handle);
+            if ~isempty(obj.mex_handle)
+                obj.AppWrapper.deleteObj(obj.mex_handle);
+            end
         end
         
         function disconnect(obj)
+            obj.saved_state = obj.getSavedState();
             if ~isempty(obj.mex_handle)
-                obj.saved_state = obj.runCommand('Serialize');
                 obj.AppWrapper.deleteObj(obj.mex_handle);
                 obj.mex_handle = [];
             end
         end
-        
-        function id = getID(obj)
-            id = obj.id;
-        end
+%         
+%         function id = getID(obj)
+%             id = obj.id;
+%         end
         
         function restoreConnection(obj)
             if isempty(obj.mex_handle)
@@ -50,8 +88,13 @@ classdef MexPPObjectWrapper < handle
         end
         
         function saved_state = getSavedState(obj)
+            if ~isempty(obj.mex_handle)
+                obj.saved_state = obj.runCommand('Serialize');
+            end
             saved_state = obj.saved_state;
         end
+        
+        
     end
     
 end
